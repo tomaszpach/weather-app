fb9eaeaf5cb0bdac5184be61e1013ec5ab0c4692
Tomasz Pach <tomasz@nurtureagency.com>
2018-07-23 17:54:17 +0200

    searchBar input value update

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // The user is able to query by city name
 // This probably needs to be a class
 // Type in search bar and update city on enter / click on button
-const searchBar = () => {
-    return (
-        <input type="search" placeholder="What are you looking for?"/>
-    )
-};
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
 
-export default searchBar;
+        this.state = {
+            searchValue: ''
+        }
+    }
+
+    handleSubmit(event) {
+        alert(this.state.searchValue);
+        event.preventDefault();
+    }
+
+    updateSearchInputValue(event) {
+        this.setState({
+            searchValue: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+                <label>
+                    City:
+                    <input onInput={(event) => this.updateSearchInputValue(event)} type="search"
+                           placeholder="What are you looking for?" value={this.state.searchValue}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
