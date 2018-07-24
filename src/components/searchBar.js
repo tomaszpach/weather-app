4720f52fb3c21d587c73d985e8b0d9c113fc9de5
Tomasz Pach <tomasz@nurtureagency.com>
2018-07-24 12:52:26 +0200

    cleanup - remove some files

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,40 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-// The user is able to query by city name
-// This probably needs to be a class
-// Type in search bar and update city on enter / click on button
-class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            searchValue: ''
-        }
-    }
-
-    handleSubmit(event) {
-        alert(this.state.searchValue);
-        event.preventDefault();
-    }
-
-    updateSearchInputValue(event) {
-        this.setState({
-            searchValue: event.target.value
-        });
-    }
-
-    render() {
-        return (
-            <form onSubmit={(event) => this.handleSubmit(event)}>
-                <label>
-                    City:
-                    <input onInput={(event) => this.updateSearchInputValue(event)} type="search"
-                           placeholder="What are you looking for?" value={this.state.searchValue}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        )
-    }
-}
+const SearchBar = ({onSubmit, onInput, value}) => (
+        <form className="search-form" onSubmit={(event) => onSubmit(event)}>
+            <label>
+                <div className="field">
+                    <p className="control has-icons-left">
+                        <input onInput={(event) => onInput(event)}
+                               value={value} type="search"
+                               className="input" placeholder="London"/>
+                        <span className="icon is-small is-left">
+                                <i className="fas fa-map-marker-alt"/>
+                            </span>
+                    </p>
+                </div>
+            </label>
+            <div className="control">
+                <button className="button is-link">Submit</button>
+            </div>
+        </form>
+    )
+;
 
 export default SearchBar;
