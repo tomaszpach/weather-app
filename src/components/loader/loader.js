0157dba4bc51f31d76e9497c21ced5f836e89b49
Tomasz Pach <tomasz@nurtureagency.com>
2018-07-24 12:52:09 +0200

    loader

---
+++ src/components/loader/loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const loader = () => (
+    <div className="spinner">
+        <div className="dot1"/>
+        <div className="dot2"/>
+    </div>
+);
+
+export default loader;
