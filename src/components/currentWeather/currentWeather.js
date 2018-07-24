3350dde0ef12b8b47f3b57f20219d249224f7d4c
Tomasz Pach <tomasz@nurtureagency.com>
2018-07-24 13:12:50 +0200

    currentWeather cleanup

--- src/components/currentWeather/currentWeather.js
+++ src/components/currentWeather/currentWeather.js
@@ -1,29 +1,11 @@
 import React from 'react';
-
 import WeatherDetails from './weatherDetails';
 
-// Get data (props) from searchBar.js input
-// Show temperature, pressure, wind, maybe some kind of icon
 const currentWeather = ({weather}) => {
-    // if (weather !== undefined) {
-    //     console.log('weather.main', weather.main);
-    //
-    //     if (weather.main) {
-    //         console.log('weather.main.temp', weather.main.temp)
-    //     }
-    // }
-
     let weatherDetails;
 
-    // todo check if there is city
-
     if (weather && weather.main) {
-        weatherDetails = (
-            <div>
-
-                <WeatherDetails name={weather.name} main={weather.main} weather={weather.weather[0]} />
-            </div>
-        );
+        weatherDetails = <WeatherDetails name={weather.name} main={weather.main} weather={weather.weather[0]}/>;
     }
 
     return (
@@ -31,7 +13,6 @@ const currentWeather = ({weather}) => {
             {weatherDetails}
         </div>
     )
-
 };
 
 export default currentWeather;
