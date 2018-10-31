4e1f27752b1aa53e9ea0a5dc7fb2aa5a379bfb94
Tomasz Pach <tomaszpach@icloud.com>
2018-10-31 11:51:22 +0100

    class component to function component with data from redux

--- src/components/weatherForecast.js
+++ src/components/weatherForecast.js
@@ -1,83 +1,56 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactHighcharts from 'react-highcharts';
 
-class WeatherForecast extends Component {
-    constructor(props) {
-        super(props);
+const WeatherForecast = ({forecast}) => {
+    let categories = [],
+        temp = [];
 
-        this.state = {
-            forecast: {}
-        }
-    }
+    if (forecast && forecast.hasOwnProperty('list')) {
+        forecast.list.map(item => {
+            let splitDt_txt = (item.dt_txt).split(' '),
+                year = splitDt_txt[0].split('2018-'),
+                hour = splitDt_txt[1].split(':');
 
-    shouldComponentUpdate(nextProps) {
-        if (nextProps.forecast.city && !(this.state.forecast.hasOwnProperty('city'))) {
-            this.setState({
-                forecast: nextProps.forecast
-            });
+            temp.push(item.main.temp);
+            categories.push(year[1] + ', ' + hour[0] + 'h');
             return true;
-        }
-
-        return false;
-    }
-
-    componentWillUnmount() {
-        this.setState({
-            forecast: {}
         });
     }
 
-    render() {
-        let categories = [],
-            temp = [];
-
-        if (this.state.forecast && this.state.forecast.hasOwnProperty('list')) {
-            this.state.forecast.list.map(item => {
-                let splitDt_txt = (item.dt_txt).split(' '),
-                    year = splitDt_txt[0].split('2018-'),
-                    hour = splitDt_txt[1].split(':');
-
-                temp.push(item.main.temp);
-                categories.push(year[1] + ', ' + hour[0] + 'h');
-                return true;
-            });
-        }
-
-        let config = {
-            chart: {
-                type: 'spline'
+    let config = {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Every 3h temperature',
+            style: {
+                color: '#808080'
             },
+        },
+        xAxis: {
+            categories: categories
+        },
+        yAxis: {
             title: {
-                text: 'Every 3h temperature',
-                style: {
-                    color: '#808080'
-                },
-            },
-            xAxis: {
-                categories: categories
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                }
-            },
-            series: [{
-                name: 'Temperature',
-                data: temp
-            }],
-            plotOptions: {
-                series: {
-                    color: '#808080'
-                }
-            },
-        };
+                text: 'Temperature (°C)'
+            }
+        },
+        series: [{
+            name: 'Temperature',
+            data: temp
+        }],
+        plotOptions: {
+            series: {
+                color: '#808080'
+            }
+        },
+    };
 
-        return (
-            <div className="forecast-weather">
-                <ReactHighcharts config={config}/>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="forecast-weather">
+            <ReactHighcharts config={config}/>
+        </div>
+    )
+};
 
 export default WeatherForecast;
