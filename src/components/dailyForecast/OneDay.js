bcb889890fdefb0e149adfe3c52156107ce2cbda
Tomasz Pach <tomaszpach@icloud.com>
2018-11-05 14:41:28 +0100

    add information about wind, clouds and humidity, styles update

--- src/components/dailyForecast/OneDay.js
+++ src/components/dailyForecast/OneDay.js
@@ -9,7 +9,7 @@ class OneDay extends React.Component {
         const {dayData, day} = this.props;
 
         return (
-            <div className="col-md-4 col-sm-4 no-padding tile-wrapper">
+            <div className="col-md-4 col-sm-4 tile-wrapper">
                 <div className="tiles red weather-widget">
                     <div className="tiles-body">
                         <NextDay dayTimestamp={dayData.dt} day={day}/>
@@ -20,15 +20,16 @@ class OneDay extends React.Component {
                             <div className="clearfix" />
                         </div>
                     </div>
-                    {/*<div className="tile-footer">*/}
-                        {/*<div className="pull-left">*/}
-                            {/*<canvas id="wind" width="32" height="32"></canvas>*/}
-                            {/*<span className="text-white small-text-description">Windy</span></div>*/}
-                        {/*<div className="pull-right">*/}
-                            {/*<canvas id="rain" width="32" height="32"></canvas>*/}
-                            {/*<span className="text-white small-text-description">Humidity</span></div>*/}
-                        {/*<div className="clearfix"></div>*/}
-                    {/*</div>*/}
+                    <div className="tile-footer">
+                        <div className="pull-left">
+                            <div>Wiatr: {dayData.speed} m/s</div>
+                            <div>Zachmurzenie: {dayData.clouds}%</div>
+                        </div>
+                        <div className="pull-right">
+                            <canvas id="rain" width="32" height="32"></canvas>
+                            <span className="text-white small-text-description">Wilgotność: {dayData.humidity}%</span></div>
+                        <div className="clearfix"></div>
+                    </div>
                 </div>
             </div>
         )
