afb93c6f99f10d7eb09e791ebf8f08e82917dd56
Tomasz Pach <tomaszpach@icloud.com>
2018-11-06 09:21:19 +0100

    rebuild input to use bootstrap version

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,15 +3,10 @@ import React from 'react';
 const SearchBar = ({onSubmit, onInput, value}) => (
         <form id="search-form" onSubmit={(e) => onSubmit(e)}>
             <label>
-                <div className="field">
-                    <p className="control has-icons-left">
-                        <input onInput={(e) => onInput(e)}
-                               defaultValue={value} type="search"
-                               className="input" placeholder="Kraków"/>
-                        <span className="icon is-small is-left">
-                                <i className="fas fa-map-marker-alt"/>
-                            </span>
-                    </p>
+                <div className="input-group">
+                    <span className="input-group-addon" id="basic-addon1"><i className="fas fa-map-marker-alt"/></span>
+                    <input type="search" className="form-control" placeholder="Kraków" aria-describedby="basic-addon1"
+                           onInput={(e) => onInput(e)} defaultValue={value}/>
                 </div>
             </label>
             <div className="control">
