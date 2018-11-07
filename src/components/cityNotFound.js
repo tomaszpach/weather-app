be01e7596e8cc7b20ea798c2e7fdd0de04ff1860
Tomasz Pach <tomaszpach@icloud.com>
2018-11-07 12:41:06 +0100

    add quotes for city

--- src/components/cityNotFound.js
+++ src/components/cityNotFound.js
@@ -4,7 +4,7 @@ const cityNotFound = ({location}) => {
     const locationUpper = location.charAt(0).toUpperCase() + location.slice(1);
     return (
         <div id="not-found">
-            <h2>Nie znalazłem pogody dla <b>{locationUpper}</b>.</h2>
+            <h2>Nie znalazłem pogody dla "<b>{locationUpper}</b>".</h2>
             <p>Spróbuj wpisać inną lokalizację, lub sprawdź czy nie masz literówki w nazwie.</p>
         </div>
     )
